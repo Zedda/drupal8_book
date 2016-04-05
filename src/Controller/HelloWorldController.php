4e33ff660c0e3ea69f3b5b77da87227424b8727a
Zedda <rachel-flowers@live.com>
2016-04-05 14:26:39 -0500

    First commit

---
+++ src/Controller/HelloWorldController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\hello_world\HelloWorldController.
+ */
+ 
+namespace Drupal\hello_world\Controller;
+ 
+use Symfony\Component\DependencyInjection\ContainerAware;
+ 
+class HelloWorldController extends ContainerAware {
+ 
+  /**
+   * Page Callback Method to Print out Hello World Text to the Screen.
+   */
+  public function myCallbackMethod() {
+    $element = array(
+      '#theme' => 'theme_hello_world_primary_page',
+      '#items' => 100,
+    );
+    return $element;
+  }
+}
